6d4709488a63831fe5c2b97d0e99337a9e8fb800
Lief Koepsel <lkoepsel@wellys.com>
2021-09-22 06:41:31 -0700

    updated fastPWM to match wellys.com code, confirmed it worked.

--- fastPWM/fastPWM.c
+++ fastPWM/fastPWM.c
@@ -1,23 +1,41 @@
-/* fastPWM(pin, value) in avr-gcc                       */
-/* This specific routine will deliver a Fast PWM signal     */
-/* Frequency: 16MHz / 256 / 64 = 976.56Hz                   */
-/* Pin 3/PD3 has a 50% duty cycle                           */
-/* Pin 11/PB3 has a 25% duty cycle                          */
+/* fastPWM: setup 8-bit Fast PWM using Timer/Counter 0                            
+
+Timer 0 Configuration, 8bit Fast PWM p113-7
+TCCR0A - Timer/Counter0 Control Register                 
+[ COM0A1 COM0A0 COM0B1 COM0B0 0 0 WGM01 WGM00 ]          
+
+TCCR0B - Timer/Counter0 Control Register                 
+[ FOC0A FOC0B 0 0 WGM02 CS02 CS01 CS20 ]                 
+
+COM0A1:0 = 10 => Clear OC0A on compare match (non-inverting)             
+COM0B1:0 = 11 => Set OC0B on compare match (inverting)
+WGM02:0 = 011 => Fast PWM, Update at Bottom, TOV=MAX   
+CS02:0 = 101 = clkio / 011 scalar 011 => 1/64                          
+
+PWM Pins p97   
+ARD5 PD5 OC2B 8bit Timer/Counter 0 OCR0B                        
+ARD6 PD6 OC2A 8bit Timer/Counter 0 OCR0A
+
+Specific to parameters below                             
+Frequency: 16MHz / 256 / 64 = 976.56Hz                  
+Pin 5/PD5/OCR0B has a 50% duty cycle or 127/255                          
+Pin 6/PD6/OCR0A has a 25% duty cycle or 63/255
+*/
 #include <avr/io.h>
 
 int main (void)
 {   
     // reset both timer/counters
-    TCCR2A = 0;
-    TCCR2B = 0; 
-    // set UNO pin 3/PD3 and pin 11/PB3 to output
-    DDRD |= _BV(DDD3);
-    DDRB |= _BV(DDB3);
+    TCCR0A = 0;
+    TCCR0B = 0; 
+    // set UNO pin 5/PD5 and pin 6/PD6 to output
+    DDRD |= _BV(DDD5);
+    DDRD |= _BV(DDD6);
  
-    // [ COM2A1 COM2A0 COM2B1 COM2B0 0 0 WGM21 WGM20 ] = %10100011
-    TCCR2A = _BV(COM2A1) | _BV(COM2B1) | _BV(WGM21) | _BV(WGM20);
-    // [ FOC2A FOC2B 0 0 WGM22 CS22 CS21 CS20 ] = %0100
-    TCCR2B = _BV(CS22);
-    OCR2A = 127;
-    OCR2B = 63;
+    // TCCR0A [ COM0A1 COM0A0 COM0B1 COM0B0 0 0 WGM21 WGM20 ] = 0b10110011
+    TCCR0A = _BV(COM0A1) | _BV(COM0B1) | _BV(COM0B0) | _BV(WGM21) | _BV(WGM20);
+    // TCCR0B [ FOC2A FOC2B 0 0 WGM22 CS22 CS21 CS20 ] = 0b00000011
+    TCCR0B = _BV(CS21) | _BV(CS20);
+    OCR0A = 63;
+    OCR0B = 127;
 }
---
+++ pinMode/pinMode.c
@@ -0,0 +1,129 @@
+#include <util/delay.h>
+#include <avr/io.h>
+
+#define BLINK_DELAY_MS 5000
+#define INPUT 0
+#define OUTPUT 1
+#define INPUT_PULLUP 2
+
+void off_led ()
+{
+    // Set the built-in LED PB5 to be an output
+    DDRB |= _BV(DDB5);
+    PORTB &= ~(_BV(PORTB5));
+}
+
+void on_led ()
+{
+    // Set the built-in LED PB5 to be an output
+    DDRB |= _BV(DDB5);
+    PORTB |= (_BV(PORTB5));
+}
+
+short int pinMode(short int apin, short int mode) {
+    short int errno = 0;
+    switch (apin) {
+        case 0:
+        if (mode == INPUT) {
+            DDRD &= ~_BV(DDD0);
+        }
+        else if (mode == OUTPUT) {
+            DDRD |= _BV(DDD0);
+        }
+        else if (mode == INPUT_PULLUP) {
+            DDRD &= ~_BV(DDD0);
+            PORTD |= _BV(DDD0);
+        }
+        else {
+            errno = 1;
+        }
+        break;
+        case 1:
+        if (mode == INPUT) {
+            DDRD &= ~_BV(DDD1);
+        }
+        else if (mode == OUTPUT) {
+            DDRD |= _BV(DDD1);
+        }
+        else if (mode == INPUT_PULLUP) {
+            DDRD &= ~_BV(DDD1);
+            PORTD |= _BV(DDD1);
+        }
+        else {
+            errno = 1;
+        }
+        break;
+        case 2:
+        if (mode == INPUT) {
+            DDRD &= ~_BV(DDD2);
+        }
+        else if (mode == OUTPUT) {
+            DDRD |= _BV(DDD2);
+        }
+        else if (mode == INPUT_PULLUP) {
+            DDRD &= ~_BV(DDD2);
+            PORTD |= _BV(DDD2);
+        }
+        else {
+            errno = 1;
+        }
+        break;
+        case 3:
+        if (mode == INPUT) {
+            DDRD &= ~_BV(DDD3);
+        }
+        else if (mode == OUTPUT) {
+            DDRD |= _BV(DDD3);
+        }
+        else if (mode == INPUT_PULLUP) {
+            DDRD &= ~_BV(DDD3);
+            PORTD |= _BV(DDD3);
+        }
+        else {
+            errno = 1;
+        }
+        break;
+        case 4:
+        if (mode == INPUT) {
+            DDRD &= ~_BV(DDD4);
+        }
+        else if (mode == OUTPUT) {
+            DDRD |= _BV(DDD4);
+        }
+        else if (mode == INPUT_PULLUP) {
+            DDRD &= ~_BV(DDD4);
+            PORTD |= _BV(DDD4);
+        }
+        else {
+            errno = 1;
+        }
+        break;
+        case 5:
+        if (mode == INPUT) {
+            DDRD &= ~_BV(DDD5);
+        }
+        else if (mode == OUTPUT) {
+            DDRD |= _BV(DDD5);
+        }
+        else if (mode == INPUT_PULLUP) {
+            DDRD &= ~_BV(DDD5);
+            PORTD |= _BV(DDD5);
+        }
+        else {
+            errno = 1;
+        }
+        break;
+    }
+    return(errno);
+}  
+
+int main (void)
+{
+    off_led();
+    pinMode(3, OUTPUT);
+    PORTD |= _BV(DDD3);
+    _delay_ms(BLINK_DELAY_MS);
+    PORTD &= ~_BV(DDD3);
+    _delay_ms(BLINK_DELAY_MS);
+    on_led();
+}
