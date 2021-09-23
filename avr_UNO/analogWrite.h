927b1a67c774f681820488d63a6ab1b951ecbd20
Lief Koepsel <lkoepsel@wellys.com>
2021-09-23 10:09:29 -0700

    added avr_UNO as library for equivilient Arduino C funtions, tested with analogWrite(), works

---
+++ avr_UNO/analogWrite.h
@@ -0,0 +1,11 @@
+#define delay 500
+#include <avr/io.h>
+#include <util/delay.h>
+
+void blink (short int times);
+
+void off_led ();
+
+void clear_all_TC(void);
+
+short int analogWrite(short int apin, short int cycle) ;
